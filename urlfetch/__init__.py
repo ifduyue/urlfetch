0f7e350570f1b2346ffe1bb61dfcccae78d47061
lyxint <lyxint@gmail.com>
2011-12-18 01:11:24 +0800

    delete some uas

--- urlfetch/__init__.py
+++ urlfetch/__init__.py
@@ -12,6 +12,6 @@
 from urlfetch import *
 from uas import randua
 
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 __author__ = 'Elyes Du <lyxint@gmail.com>'
 __url__ = 'https://github.com/lyxint/urlfetch'
