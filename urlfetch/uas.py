2464f67060eeb2f058f2333d13e9020f84c44818
lyxint <lyxint@gmail.com>
2012-01-17 13:54:26 +0800

    optimize

--- urlfetch/uas.py
+++ urlfetch/uas.py
@@ -1283,9 +1283,8 @@ _uas = (
     '''Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; MS-RTC LM 8)''',
 )
 
+from random import choice
 
 def randua():
     '''Returns a User-Agent string randomly'''
-
-    from random import choice
     return choice(_uas)
