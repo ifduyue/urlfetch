57f3baa971ded203d787eab7d1fe4d530a252827
lyxint <lyxint@gmail.com>
2012-01-17 13:54:48 +0800

    file upload

--- urlfetch/__init__.py
+++ urlfetch/__init__.py
@@ -9,13 +9,13 @@
 #    :license: BSD, see LICENSE for more details.
 #
 
-__version__ = '0.1.11'
+__version__ = '0.2'
 __author__ = 'Elyes Du <lyxint@gmail.com>'
 __url__ = 'https://github.com/lyxint/urlfetch'
 
 
 import httplib
-from urllib import urlencode
+from urllib import urlencode, quote as urlquote, quote_plus as urlquote_plus
 import urlparse
 import Cookie
 from uas import randua
@@ -46,7 +46,64 @@ def sc2cs(sc):
     return '; '.join(sc)
 
 
-def fetch(url, data=None, headers={}, timeout=None, randua=True):
+_boundary_prefix = None
+def choose_boundary():
+    global _boundary_prefix
+    if _boundary_prefix is None:
+        _boundary_prefix = "urlfetch"
+        import os
+        try:
+            uid = repr(os.getuid())
+            _boundary_prefix += "." + uid
+        except AttributeError: pass
+        try:
+            pid = repr(os.getpid())
+            _boundary_prefix += "." + pid
+        except AttributeError: pass
+    import uuid
+    return "%s.%s" % (_boundary_prefix, uuid.uuid4().hex)
+
+def _encode_multipart(data, files):
+    parts = []
+    boundary = choose_boundary()
+    part_boundary = '--' + boundary
+
+    for name, value in data.iteritems():
+        parts.extend([
+            part_boundary,
+            'Content-Disposition: form-data; name="%s"' % name,
+            '',
+            str(value),
+        ])
+
+    for name, f in files.iteritems():
+        if hasattr(f, 'name'):
+            filename = f.name
+        else:
+            filename = name
+        if hasattr(f, 'read'):
+            value = f.read()
+        elif isinstance(f, basestring):
+            value = f
+        else:
+            value = str(f)
+
+        parts.extend([
+            part_boundary,
+            'Content-Disposition: form-data; name="%s"; filename="%s"' % (name, filename),
+            'Content-Type: application/octet-stream',
+            '',
+            value,
+        ])
+    parts.append('--' + boundary + '--')
+    parts.append('')
+
+    body = '\r\n'.join(parts)
+    content_type = 'multipart/form-data; boundary=%s' % boundary
+
+    return content_type, body
+
+def fetch(url, data=None, headers={}, timeout=None, randua=True, files=[]):
     ''' fetch url
 
     Args:
@@ -74,7 +131,7 @@ def fetch(url, data=None, headers={}, timeout=None, randua=True):
 
 
 
-def fetch2(url, method="GET", data=None, headers={}, timeout=None, randua=True):
+def fetch2(url, method="GET", data=None, headers={}, timeout=None, randua=True, files=[]):
     ''' fetch url
 
     Args:
@@ -136,10 +193,13 @@ def fetch2(url, method="GET", data=None, headers={}, timeout=None, randua=True):
     }
     if auth: reqheaders['Authorization'] = 'Basic %s' % auth
 
-    if isinstance(data, dict):
+    if data is not None and files:
+        content_type, data = _encode_multipart(data, files)
+        reqheaders['Content-Type'] = content_type
+    elif isinstance(data, dict):
         data = urlencode(data)
     
-    if isinstance(data, basestring) and method in ("POST", "PUT"):
+    if isinstance(data, basestring) and not files:
         # httplib will set 'Content-Length', also you can set it by yourself
         reqheaders["Content-Type"] = "application/x-www-form-urlencoded"
         # what if the method is GET, HEAD or DELETE 
