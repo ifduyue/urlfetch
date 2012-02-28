2adf8137674629bcd04aa73bfbab7b07b0912c81
lyxint <lyxint@gmail.com>
2012-02-28 19:58:56 +0800

    support for python 3

--- urlfetch/__init__.py
+++ urlfetch/__init__.py
@@ -9,7 +9,7 @@
 #    :license: BSD, see LICENSE for more details.
 #
 
-__version__ = '0.2.3'
+__version__ = '0.3.0'
 __author__ = 'Elyes Du <lyxint@gmail.com>'
 __url__ = 'https://github.com/lyxint/urlfetch'
 
@@ -23,15 +23,27 @@ if util.py3k:
     import urllib.parse as urlparse
     import http.cookies as Cookie
     basestring = (str, bytes)
+    def b(s):
+        return s.encode('latin-1')
+    def u(s):
+        return s
 else:
     from httplib import HTTPConnection, HTTPException
     from httplib import HTTP_PORT, HTTPS_PORT
     from urllib import urlencode, quote as urlquote, quote_plus as urlquote_plus
     import urlparse
     import Cookie
+    def b(s):
+        return s
+    def u(s):
+        return unicode(s, "unicode_escape")
+
 import base64
 from functools import partial
 import os
+from io import BytesIO
+import codecs
+writer = codecs.lookup('utf-8')[3]
 
 
 __all__ = [
@@ -39,6 +51,8 @@ __all__ = [
     'get', 'head', 'put', 'post', 'delete', 'options',
     'UrlfetchException',
 ] 
+
+_allowed_methods = ("GET", "DELETE", "HEAD", "OPTIONS", "PUT", "POST", "TRACE", "PATCH")
 
 class UrlfetchException(Exception): pass
 
@@ -78,18 +92,22 @@ def choose_boundary():
     return "(*^__^*)%s.%s" % (_boundary_prefix, uuid.uuid4().hex)
 
 def _encode_multipart(data, files):
+    body = BytesIO()
     parts = []
     boundary = choose_boundary()
-    part_boundary = '--' + boundary
+    part_boundary = b('--%s\r\n' % boundary)
 
     if isinstance(data, dict):
         for name, value in data.items():
-            parts.extend([
-                part_boundary,
-                'Content-Disposition: form-data; name="%s"' % name,
-                '',
-                str(value),
-            ])
+            body.write(part_boundary)
+            writer(body).write('Content-Disposition: form-data; name="%s"\r\n' % name)
+            body.write(b'Content-Type: text/plain\r\n\r\n')
+            if util.py3k and isinstance(value, str): 
+                writer(body).write(value)
+            else:
+                body.write(value)
+            body.write(b'\r\n')
+            
 
     for fieldname, f in files.items():
         if isinstance(f, tuple):
@@ -106,22 +124,28 @@ def _encode_multipart(data, files):
             value = f
         else:
             value = str(f)
+
+        body.write(part_boundary)
         if filename:
-            parts.extend([
-                part_boundary,
-                'Content-Disposition: form-data; name="%s"; filename="%s"' % (fieldname, filename),
-                'Content-Type: application/octet-stream',
-                '',
-                value,
-            ])
+            writer(body).write('Content-Disposition: form-data; name="%s"; filename="%s"\r\n' % (fieldname, filename))
+            body.write(b'Content-Type: application/octet-stream\r\n\r\n')
+        else:
+            writer(body).write('Content-Disposition: form-data; name="%s"\r\n' % name)
+            body.write(b'Content-Type: text/plain\r\n\r\n')
 
-    parts.append('--' + boundary + '--')
-    parts.append('')
+        if util.py3k and isinstance(value, str):
+            writer(body).write(value)
+        else:
+            body.write(value)
+        body.write(b'\r\n')
 
-    body = '\r\n'.join(parts)
+    body.write(b('--' + boundary + '--\r\n'))
+
     content_type = 'multipart/form-data; boundary=%s' % boundary
+    #body.write(b(content_type))
 
-    return content_type, body
+    return content_type, body.getvalue()
+
 
 def fetch(url, data=None, headers={}, timeout=None, randua=True, files={}, auth=None):
     ''' fetch url
@@ -187,8 +211,8 @@ def request(url, method="GET", data=None, headers={}, timeout=None, randua=True,
 
     scheme, netloc, path, query, fragment = urlparse.urlsplit(url)
     method = method.upper()
-    if method not in ("GET", "PUT", "DELETE", "POST", "HEAD", "OPTIONS"):
-        method = "GET"
+    if method not in _allowed_methods:
+        raise UrlfetchException("Method shoud be one of " + ", ".join(_allowed_methods))
 
     requrl = path
     if query: requrl += '?' + query
@@ -222,10 +246,8 @@ def request(url, method="GET", data=None, headers={}, timeout=None, randua=True,
     if auth is not None: 
         if isinstance(auth, (list, tuple)):
             auth = '%s:%s' % tuple(auth)
-        if util.py3k:
-            auth = auth.encode('utf-8')
-        auth = base64.b64encode(auth)
-        reqheaders['Authorization'] = b'Basic ' + auth
+        auth = base64.b64encode(auth.encode('utf-8'))
+        reqheaders['Authorization'] = 'Basic ' + auth.decode('utf-8')
 
     if files:
         content_type, data = _encode_multipart(data, files)
@@ -251,11 +273,13 @@ def request(url, method="GET", data=None, headers={}, timeout=None, randua=True,
     
     return response
 
-# some convient functions
+# some shortcuts
 get = partial(request, method="GET")
 post = partial(request, method="POST")
 put = partial(request, method="PUT")
 delete = partial(request, method="DELETE")
 head = partial(request, method="HEAD")
 options = partial(request, method="OPTIONS")
+trace = partial(request, method="TRACE")
+patch = partial(request, method="PATCH")
 
