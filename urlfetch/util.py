381e3a41528012c33ea37da98c51f785aecdd6fd
lyxint <lyxint@gmail.com>
2012-04-05 11:17:23 +0800

    mb_code, decoding/encoding func
    mb_code is cheap, working on a better solution

--- urlfetch/util.py
+++ urlfetch/util.py
@@ -3,5 +3,16 @@ import sys
 
 if sys.version_info >= (3, 0):
     py3k = True
+    unicode = str
 else:
     py3k = False
+
+def mb_code(s, coding=None):
+    if isinstance(s, unicode):
+        return s if coding is None else s.encode(coding)
+    for c in ('utf-8', 'gb2312', 'gbk', 'gb18030', 'big5'):
+        try:
+            s = s.decode(c, errors='replace')
+            return s if coding is None else s.encode(coding, errors='replace')
+        except: pass
+    return s
