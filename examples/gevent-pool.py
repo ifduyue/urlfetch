b8be2518dd56f1628a01d10979e18f9d5a85d561
Andrey Usov <ownport@gmail.com>
2012-08-16 15:21:48 +0300

    added example how to use urlfetch in pool with gevent

---
+++ examples/gevent-pool.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2
+#
+#   Example of usage pool with gevent
+#
+
+from gevent import monkey
+monkey.patch_socket()
+
+from gevent.pool import Pool
+import urlfetch
+
+pool = Pool(size=5)
+urls = ('http://www.google.com', 'http://www.yahoo.com', 'http://www.blogger.com',
+        'http://www.python.org', 'http://sourceforge.net', 'http://www.ubuntu.com', 
+        'http://www.readwriteweb.com', 'http://gigaom.com', 'http://www.wired.com', )
+
+tasks = pool.map(urlfetch.get, urls)
+
